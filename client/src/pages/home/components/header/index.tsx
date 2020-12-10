8382630088f38622e994202d95ed5313f3fe5afc
miaoyu <miaoyu@sefonsoft.com>
2020-12-10 14:44:41 +0800

    search component

--- client/src/pages/home/components/header/index.tsx
+++ client/src/pages/home/components/header/index.tsx
@@ -5,7 +5,7 @@ interface IHeaderProps { }
 
 const Header: React.FC<IHeaderProps> = (props) => {
   return <div className="header">
-    <div className="header_title">homestay</div>
+    <div className="header_title">Homestay</div>
     <div className='header_login'>
       <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
     </div>
--- client/src/pages/home/components/search/index.tsx
+++ client/src/pages/home/components/search/index.tsx
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { Picker, List, Button, Calendar } from "antd-mobile";
+import dayjs from "dayjs";
 
-interface ISearchProps {}
+interface ISearchProps { }
 
-const Search:React.FC<ISearchProps> = (props) => {
-    return <div>search</div>
+const DATE_FORMATE = 'YYYY-MM-DD'
+
+const Search: React.FC<ISearchProps> = (props) => {
+  const [cities, setCities] = React.useState([[{ label: '杭州', value: '10001' }, { label: '苏州', value: '10002' }]])
+  const [selectedCites, setSelectedCites] = React.useState(["10001"])
+  const [calendarVisible, setCalendarVisible] = React.useState(false)
+  const [date, setDate] = React.useState("select")
+
+  const handleCalendarVisible = () => {
+    setCalendarVisible(!calendarVisible)
+  }
+
+  const handleCalendarConfirm = (startTime?: Date, endTime?: Date) => {
+    handleCalendarVisible()
+    setDate(
+      `${dayjs(startTime).format(DATE_FORMATE)}~${dayjs(endTime).format(DATE_FORMATE)}`
+    )
+  }
+  
+  const handleCityChange = (value: any) => {
+    console.log(value)
+    setSelectedCites(value)
+  }
+
+  return <div className="search">
+    {/* optional cities */}
+    <div className="search-addr">
+      <Picker
+        title="city"
+        data={cities}
+        value={selectedCites}
+        cascade={false}
+        cols={1}
+        onChange={handleCityChange}
+      >
+        <List.Item>Optional City</List.Item>
+      </Picker>
+    </div>
+    {/* optional times */}
+    {/* <List.Item onClick={handleCalendarVisible} extra={date}>Date</List.Item> */}
+    <div className='search-time' onClick={handleCalendarVisible}>
+      <p className='search-time_left'>Date</p>
+      <p className='search-time_right'>{date}</p>
+    </div>{/* click button */}
+    <Button size="large" type="warning">Search</Button>
+    <Calendar
+      visible={calendarVisible}
+      onCancel={handleCalendarVisible}
+      onConfirm={handleCalendarConfirm}
+    />
+  </div>
 }
 
 export default Search
