d84c55883d1269d69848009146788e538e49906f
miaoyu <miaoyu@sefonsoft.com>
2020-12-19 13:58:35 +0800

    add tabs

--- client/src/pages/order/index.tsx
+++ client/src/pages/order/index.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Tabs } from "antd-mobile";
+import "./index.less"
 
-interface IOrderProps {}
+interface IOrderProps { }
 
-const Order:React.FC<IOrderProps> = (props) => {
-    return <div>order</div>
+const Order: React.FC<IOrderProps> = (props) => {
+
+    const tabs = [
+        { title: 'Paid', sub: 0 },
+        { title: 'Unpaid', sub: 1 },
+    ];
+    return (
+        <div className="order-page">
+
+            <Tabs tabs={tabs}
+            >
+                <div className="tab"> 1</div>
+                <div className="tab"> 2 </div>
+            </Tabs>
+        </div>
+    )
 }
 
 export default Order
