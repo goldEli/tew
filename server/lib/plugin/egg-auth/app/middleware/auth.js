76094f4789d8bef7e111afb15cdae4cd18c37e30
miaoyu <miaoyu@sefonsoft.com>
2020-12-23 15:38:31 +0800

    login

--- server/lib/plugin/egg-auth/app/middleware/auth.js
+++ server/lib/plugin/egg-auth/app/middleware/auth.js
@@ -2,8 +2,10 @@ module.exports = (options) => {
   return async (ctx, next) => {
     const url = ctx.request.url.split("?")[0];
     // const user = ctx.session[ctx.username]
-    const user = await ctx.app.redis.get(ctx.username);
-    if (!user && !options.exclude.includes(url)) {
+    const token = ctx.request.token
+    const username = await ctx.app.redis.get(ctx.username);
+    const isUser = token === username 
+    if (!isUser && !options.exclude.includes(url)) {
       ctx.body = {
         status: 1001,
         errMsg: "User not log in",
